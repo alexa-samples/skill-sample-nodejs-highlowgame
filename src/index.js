357cf24099530919af0cf8c19a8e8d0b017dbcb8
Diego Benitez <diegoben@amazon.com>
2016-07-15 10:45:49 -0700

    Initial commit of High-low guessing game.

---
+++ src/index.js
@@ -0,0 +1,113 @@
+'use strict';
+var Alexa = require("alexa-sdk");
+var appId = ''; //'amzn1.echo-sdk-ams.app.your-skill-id';
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.appId = appId;
+    alexa.dynamoDBTableName = 'highLowGuessUsers';
+    alexa.registerHandlers(newSessionHandlers, guessModeHandlers, startGameHandlers, guessAttemptHandlers);
+    alexa.execute();
+};
+
+var states = {
+    GUESSMODE: '_GUESSMODE', // User is trying to guess the number.
+    STARTMODE: '_STARTMODE'  // Prompt the user to start or restart the game.
+};
+
+var newSessionHandlers = {
+    'NewSession': function() {
+        if(Object.keys(this.attributes).length === 0) {
+            this.attributes['endedSessionCount'] = 0;
+            this.attributes['gamesPlayed'] = 0;
+        }
+        this.handler.state = states.STARTMODE;
+        this.emit(':ask', 'Welcome to High Low guessing game. You have played '
+            + this.attributes['gamesPlayed'].toString() + ' times. would you like to play?',
+            'Say yes to start the game or no to quit.');
+    }
+};
+
+var startGameHandlers = Alexa.CreateStateHandler(states.STARTMODE, {
+    'NewSession': function () {
+        this.emit('NewSession'); // Uses the handler in newSessionHandlers
+    },
+    'AMAZON.HelpIntent': function() {
+        var message = 'I will think of a number between zero and one hundred, try to guess and I will tell you if it' +
+            ' is higher or lower. Do you want to start the game?';
+        this.emit(':ask', message, message);
+    },
+    'AMAZON.YesIntent': function() {
+        this.attributes["guessNumber"] = Math.floor(Math.random() * 100);
+        this.handler.state = states.GUESSMODE;
+        this.emit(':ask', 'Great! ' + 'Try saying a number to start the game.', 'Try saying a number.');
+    },
+    'AMAZON.NoIntent': function() {
+        this.emit(':tell', 'Ok, see you next time!');
+    },
+    'SessionEndedRequest': function () {
+        console.log('session ended!');
+        this.attributes['endedSessionCount'] += 1;
+        this.emit(':saveState', true);
+    },
+    'Unhandled': function() {
+        var message = 'Say yes to continue, or no to end the game.';
+        this.emit(':ask', message, message);
+    }
+});
+
+var guessModeHandlers = Alexa.CreateStateHandler(states.GUESSMODE, {
+    'NewSession': function () {
+        this.handler.state = '';
+        this.emitWithState('NewSession'); // Equivalent to the Start Mode NewSession handler
+    },
+    'NumberGuessIntent': function() {
+        var guessNum = parseInt(this.event.request.intent.slots.number.value);
+        var targetNum = this.attributes["guessNumber"];
+        console.log('user guessed: ' + guessNum);
+
+        if(guessNum > targetNum){
+            this.emit('TooHigh', guessNum);
+        } else if( guessNum < targetNum){
+            this.emit('TooLow', guessNum);
+        } else if (guessNum === targetNum){
+            // With a callback, use the arrow function to preserve the correct 'this' context
+            this.emit('JustRight', () => {
+                this.emit(':ask', guessNum.toString() + 'is correct! Would you like to play a new game?',
+                'Say yes to start a new game, or no to end the game.');
+        })
+        } else {
+            this.emit('NotANum');
+        }
+    },
+    'AMAZON.HelpIntent': function() {
+        this.emit(':ask', 'I am thinking of a number between zero and one hundred, try to guess and I will tell you' +
+            ' if it is higher or lower.', 'Try saying a number.');
+    },
+    'SessionEndedRequest': function () {
+        console.log('session ended!');
+        this.attributes['endedSessionCount'] += 1;
+        this.emit(':saveState', true);
+    },
+    'Unhandled': function() {
+        this.emit(':ask', 'Sorry, I didn\'t get that. Try saying a number.', 'Try saying a number.');
+    }
+});
+
+// These handlers are not bound to a state
+var guessAttemptHandlers = {
+    'TooHigh': function(val) {
+        this.emit(':ask', val.toString() + ' is too high.', 'Try saying a smaller number.');
+    },
+    'TooLow': function(val) {
+        this.emit(':ask', val.toString() + ' is too low.', 'Try saying a larger number.');
+    },
+    'JustRight': function(callback) {
+        this.handler.state = states.STARTMODE;
+        this.attributes['gamesPlayed']++;
+        callback();
+    },
+    'NotANum': function() {
+        this.emit(':ask', 'Sorry, I didn\'t get that. Try saying a number.', 'Try saying a number.');
+    }
+};
