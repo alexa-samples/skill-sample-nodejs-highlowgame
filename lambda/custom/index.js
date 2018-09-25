a2a1aa89d5ca41599d969da94e7b12b977681e5e
Jake Kelly <jakemkelly84@gmail.com>
2018-09-25 14:48:53 -0600

    Update index to use i18next

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -2,13 +2,10 @@
 /* eslint-disable  no-console */
 /* eslint-disable  no-restricted-syntax */
 
-const Alexa = require('ask-sdk');
+const Alexa = require('ask-sdk-core');
+const i18n = require('i18next');
+const sprintf = require('i18next-sprintf-postprocessor');
 
-const SKILL_NAME = 'High Low Game';
-const FALLBACK_MESSAGE_DURING_GAME = `The ${SKILL_NAME} skill can't help you with that.  Try guessing a number between 0 and 100. `;
-const FALLBACK_REPROMPT_DURING_GAME = 'Please guess a number between 0 and 100.';
-const FALLBACK_MESSAGE_OUTSIDE_GAME = `The ${SKILL_NAME} skill can't help you with that.  It will come up with a number between 0 and 100 and you try to guess it by saying a number in that range. Would you like to play?`;
-const FALLBACK_REPROMPT_OUTSIDE_GAME = 'Say yes to start the game or no to quit.';
 
 const LaunchRequest = {
   canHandle(handlerInput) {
@@ -29,8 +26,9 @@ const LaunchRequest = {
 
     attributesManager.setSessionAttributes(attributes);
 
-    const speechOutput = `Welcome to High Low guessing game. You have played ${attributes.gamesPlayed.toString()} times. would you like to play?`;
-    const reprompt = 'Say yes to start the game or no to quit.';
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    const speechOutput = requestAttributes.t('LAUNCH_FIRST') + attributes.gamesPlayed.toString() + requestAttributes.t('LAUNCH_SECOND');
+    const reprompt = requestAttributes.t('LAUNCH_REPROMPT');
     return responseBuilder
       .speak(speechOutput)
       .reprompt(reprompt)
@@ -47,8 +45,9 @@ const ExitHandler = {
         || request.intent.name === 'AMAZON.StopIntent');
   },
   handle(handlerInput) {
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
     return handlerInput.responseBuilder
-      .speak('Thanks for playing!')
+      .speak(requestAttributes.t('EXIT_MESSAGE'))
       .getResponse();
   },
 };
@@ -70,13 +69,10 @@ const HelpIntent = {
     return request.type === 'IntentRequest' && request.intent.name === 'AMAZON.HelpIntent';
   },
   handle(handlerInput) {
-    const speechOutput = 'I am thinking of a number between zero and one hundred, try to guess it and I will tell you' +
-            ' if it is higher or lower.';
-    const reprompt = 'Try saying a number.';
-
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
     return handlerInput.responseBuilder
-      .speak(speechOutput)
-      .reprompt(reprompt)
+      .speak(requestAttributes.t('HELP_MESSAGE'))
+      .reprompt(requestAttributes.t('HELP_REPROMPT'))
       .getResponse();
   },
 };
@@ -100,13 +96,14 @@ const YesIntent = {
     const attributesManager = handlerInput.attributesManager;
     const responseBuilder = handlerInput.responseBuilder;
     const sessionAttributes = attributesManager.getSessionAttributes();
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
 
     sessionAttributes.gameState = 'STARTED';
     sessionAttributes.guessNumber = Math.floor(Math.random() * 101);
 
     return responseBuilder
-      .speak('Great! Try saying a number to start the game.')
-      .reprompt('Try saying a number.')
+      .speak(requestAttributes.t('YES_MESSAGE'))
+      .reprompt(requestAttributes.t('HELP_REPROMPT'))
       .getResponse();
   },
 };
@@ -130,6 +127,7 @@ const NoIntent = {
     const attributesManager = handlerInput.attributesManager;
     const responseBuilder = handlerInput.responseBuilder;
     const sessionAttributes = attributesManager.getSessionAttributes();
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
 
     sessionAttributes.endedSessionCount += 1;
     sessionAttributes.gameState = 'ENDED';
@@ -137,7 +135,10 @@ const NoIntent = {
 
     await attributesManager.savePersistentAttributes();
 
-    return responseBuilder.speak('Ok, see you next time!').getResponse();
+    return handlerInput.responseBuilder
+      .speak(requestAttributes.t('STOP_MESSAGE'))
+      .getResponse();
+
   },
 };
 
@@ -146,10 +147,11 @@ const UnhandledIntent = {
     return true;
   },
   handle(handlerInput) {
-    const outputSpeech = 'Say yes to continue, or no to end the game.';
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+
     return handlerInput.responseBuilder
-      .speak(outputSpeech)
-      .reprompt(outputSpeech)
+      .speak(requestAttributes.t('UNHANDLED_RESPONSE'))
+      .reprompt(requestAttributes.t('UNHANDLED_RESPONSE'))
       .getResponse();
   },
 };
@@ -175,30 +177,32 @@ const NumberGuessIntent = {
     const guessNum = parseInt(requestEnvelope.request.intent.slots.number.value, 10);
     const sessionAttributes = attributesManager.getSessionAttributes();
     const targetNum = sessionAttributes.guessNumber;
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+
 
     if (guessNum > targetNum) {
-      return responseBuilder
-        .speak(`${guessNum.toString()} is too high.`)
-        .reprompt('Try saying a smaller number.')
+      return handlerInput.responseBuilder
+        .speak(guessNum.toString() + requestAttributes.t('TOO_HIGH_MESSAGE')
+        .reprompt(requestAttributes.t('TOO_HIGH_REPROMPT')
         .getResponse();
     } else if (guessNum < targetNum) {
-      return responseBuilder
-        .speak(`${guessNum.toString()} is too low.`)
-        .reprompt('Try saying a larger number.')
+      return handlerInput.responseBuilder
+        .speak(guessNum.toString() + requestAttributes.t('TOO_LOW_MESSAGE')
+        .reprompt(requestAttributes.t('TOO_LOW_REPROMPT')
         .getResponse();
     } else if (guessNum === targetNum) {
       sessionAttributes.gamesPlayed += 1;
       sessionAttributes.gameState = 'ENDED';
       attributesManager.setPersistentAttributes(sessionAttributes);
       await attributesManager.savePersistentAttributes();
-      return responseBuilder
-        .speak(`${guessNum.toString()} is correct! Would you like to play a new game?`)
-        .reprompt('Say yes to start a new game, or no to end the game.')
+      return handlerInput.responseBuilder
+        .speak(guessNum.toString() + requestAttributes.t('GUESS_CORRECT_MESSAGE')
+        .reprompt(requestAttributes.t('GUESS_CORRECT_REPROMPT'))
         .getResponse();
     }
     return handlerInput.responseBuilder
-      .speak('Sorry, I didn\'t get that. Try saying a number.')
-      .reprompt('Try saying a number.')
+      .speak(requestAttributes.t('FALLBACK_MESSAGE_DURING_GAME'))
+      .reprompt(requestAttributes.t('FALLBACK_REPROMPT_DURING_GAME'))
       .getResponse();
   },
 };
@@ -209,10 +213,11 @@ const ErrorHandler = {
   },
   handle(handlerInput, error) {
     console.log(`Error handled: ${error.message}`);
-
+    console.log(`Error stack: ${error.stack}`);
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
     return handlerInput.responseBuilder
-      .speak('Sorry, I can\'t understand the command. Please say again.')
-      .reprompt('Sorry, I can\'t understand the command. Please say again.')
+      .speak(requestAttributes.t('ERROR_MESSAGE'))
+      .reprompt(requestAttributes.t('ERROR_MESSAGE'))
       .getResponse();
   },
 };
@@ -239,16 +244,45 @@ const FallbackHandler = {
       // currently playing
 
       return handlerInput.responseBuilder
-        .speak(FALLBACK_MESSAGE_DURING_GAME)
-        .reprompt(FALLBACK_REPROMPT_DURING_GAME)
+        .speak(requestAttributes.t('FALLBACK_MESSAGE_DURING_GAME')
+        .reprompt(requestAttributes.t('FALLBACK_REPROMPT_DURING_GAME')
         .getResponse();
     }
 
     // not playing
     return handlerInput.responseBuilder
-      .speak(FALLBACK_MESSAGE_OUTSIDE_GAME)
-      .reprompt(FALLBACK_REPROMPT_OUTSIDE_GAME)
+      .speak(requestAttributes.t('FALLBACK_MESSAGE_OUTSIDE_GAME')
+      .reprompt(requestAttributes.t('FALLBACK_REPROMPT_OUTSIDE_GAME')
       .getResponse();
+  },
+};
+
+const LocalizationInterceptor = {
+  process(handlerInput) {
+    const localizationClient = i18n.use(sprintf).init({
+      lng: handlerInput.requestEnvelope.request.locale,
+      resources: languageStrings,
+    });
+    localizationClient.localize = function localize() {
+      const args = arguments;
+      const values = [];
+      for (let i = 1; i < args.length; i += 1) {
+        values.push(args[i]);
+      }
+      const value = i18n.t(args[0], {
+        returnObjects: true,
+        postProcess: 'sprintf',
+        sprintf: values,
+      });
+      if (Array.isArray(value)) {
+        return value[Math.floor(Math.random() * value.length)];
+      }
+      return value;
+    };
+    const attributes = handlerInput.attributesManager.getRequestAttributes();
+    attributes.t = function translate(...args) {
+      return localizationClient.localize(...args);
+    };
   },
 };
 
@@ -266,7 +300,76 @@ exports.handler = skillBuilder
     FallbackHandler,
     UnhandledIntent,
   )
+  .addRequestInterceptors(LocalizationInterceptor)
   .addErrorHandlers(ErrorHandler)
   .withTableName('High-Low-Game')
   .withAutoCreateTable(true)
   .lambda();
+
+// translations
+
+const enData = {
+  translation: {
+    SKILL_NAME: 'High Low Game',
+    EXIT_MESSAGE: 'Thanks for playing!',
+    FALLBACK_MESSAGE_DURING_GAME: `The ${SKILL_NAME} skill can't help you with that.  Try guessing a number between 0 and 100. `,
+    FALLBACK_REPROMPT_DURING_GAME: 'Please guess a number between 0 and 100.',
+    FALLBACK_MESSAGE_OUTSIDE_GAME: `The ${SKILL_NAME} skill can't help you with that.  It will come up with a number between 0 and 100 and you try to guess it by saying a number in that range. Would you like to play?`,
+    FALLBACK_REPROMPT_OUTSIDE_GAME: 'Say yes to start the game or no to quit.',
+    GUESS_CORRECT_MESSAGE: ' is correct! Would you like to play a new game?',
+    GUESS_CORRECT_REPROMPT: 'Say yes to start a new game, or no to end the game.'
+    LAUNCH_FIRST: 'Welcome to High Low guessing game. You have played ',
+    LAUNCH_SECOND: ' times. would you like to play?',
+    LAUNCH_REPROMPT: 'Say yes to start the game or no to quit.',
+    TOO_HIGH_MESSAGE: ' is too high.',
+    TOO_HIGH_REPROMPT: 'Try guessing a smaller number.',
+    TOO_LOW_MESSAGE: ' is too low.',
+    TOO_LOW_REPROMPT: 'Try guessing a larger number.',
+    HELP_MESSAGE: 'I am thinking of a number between zero and one hundred, try to guess it and I will tell you if it is higher or lower.',
+    HELP_REPROMPT: 'Try saying a number.',
+    ERROR_MESSAGE: 'Sorry, an error occurred.',
+    UNHANDLED_RESPONSE: 'Say yes to continue, or no to end the game.',
+    STOP_MESSAGE: 'Ok, see you next time!'
+  },
+};
+
+const enauData = {
+  translation: {
+    SKILL_NAME: 'Austrailian High Low Game',
+  },
+};
+
+const encaData = {
+  translation: {
+    SKILL_NAME: 'Canadian High Low Game',
+  },
+};
+
+const engbData = {
+  translation: {
+    SKILL_NAME: 'British High Low Game',
+  },
+};
+
+const eninData = {
+  translation: {
+    SKILL_NAME: 'Indian High Low Game',
+  },
+};
+
+const enusData = {
+  translation: {
+    SKILL_NAME: 'American High Low Game',
+  },
+};
+
+// constructs i18n and l10n data structure
+// translations for this sample can be found at the end of this file
+const languageStrings = {
+  'en': enData,
+  'en-AU': enauData,
+  'en-CA': encaData,
+  'en-GB': engbData,
+  'en-IN': eninData,
+  'en-US': enusData
+};
